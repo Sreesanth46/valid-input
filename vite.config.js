f6e39c2077c6c5022868ab2e4b87165f19d8d155
Sreesanth <sreesanth@innovaturelabs.com>
2023-04-03 16:23:31 +0530

    feat: vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve, dirname, format } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: resolve(dirname(''), 'src/main.js'),
+            name: 'v-input',
+            fileName: (format) => `${format}.js`
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    }
 })
